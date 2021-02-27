83489fa02c79e371b27b71c1c0a5a964b7394eb3
maifeeulasad <maifeeulasad@gmail.com>
2021-02-27 14:38:17 +0600

    on url change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 import './App.css';
 
+//const fs = require('fs');
+//const ytdl = require('ytdl-core');
+
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      url:'',
+    }
+  }
+
+  onUrlChange = (e) => {
+    this.setState({url:e.target.value},()=>{console.log(this.state.url)})
+  }
 
   render(){
     return(
-      <div>
-        <form action="" class="search-bar">
-	        <input type="search" name="search" pattern=".*\S.*" required/>
-	        <button class="search-btn" type="submit">
+      <div className="center">
+        <form action="" className="search-bar">
+	        <input required onChange={(e)=>{this.onUrlChange(e)}}/>
+	        <button className="search-btn" type="submit">
 		        <span>Search</span>
 	        </button>
         </form>
